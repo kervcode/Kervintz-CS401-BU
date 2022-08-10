94ac98cee728f49e8f82f842f0fd2b034e92c2dc
kervcodes <kervintz84@hotmail.com>
2022-08-10 19:31:47 -0400

    js converter done

---
+++ F2C/js.js
@@ -0,0 +1,17 @@
+let fahrenheit = prompt("Enter Fahrenheit temperature");
+
+
+
+
+// step 2
+do {
+    if(fahrenheit < -100 || fahrenheit > 212){
+      alert("You entered " + fahrenheit + ". \n Entry must range from -100 to +212");
+      fahrenheit = prompt("Enter Fahrenheit temperature");
+    } else {
+      let celcius = parseFloat((fahrenheit - 32)  * (5/9)).toFixed(2);
+      
+      alert("Fahrenheit temparatur is " + fahrenheit + "\n\n Celcius temperature is " + celcius);
+      fahrenheit = prompt("Enter Fahrenheit temperature");
+    }
+} while (fahrenheit != 999);
