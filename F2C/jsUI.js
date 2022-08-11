0bd1ad0f1a59e4c0b5eb10d75028d68f53d8cea4
kervcodes <kervintz84@hotmail.com>
2022-08-11 19:20:07 -0400

    finish temp converter

--- F2C/jsUI.js
+++ F2C/jsUI.js
@@ -1,10 +1,24 @@
+ "use strict";
 // let fahrenheit = prompt("Enter Fahrenheit temperature");
 
-let fdegrees = document.getElementById("fInput").innerText;
-let cdegrees = document.getElementById("cInput").innerText;
+// let fdegrees = document.getElementById("fInput");
+// let cdegrees = document.getElementById("cInput");
+
+// const btn = document.getElementById("btn")
 
 
-console.log(fdegrees)
+// console.log(btn.innerText);
+
+
+// function convert() {
+//   let fdegrees = document.getElementById("fInput").innerHTML;
+// let cdegrees = document.getElementById("cInput");
+
+// alert(fdegrees);
+  
+// }
+
+
 
 // // step 2
 // while (fahrenheit != 999); {
@@ -18,3 +32,52 @@ console.log(fdegrees)
 //       fahrenheit = prompt("Enter Fahrenheit temperature");
 //     }
 // } 
+
+
+var $ = function(id) { return document.getElementById(id); };
+
+var calculate = function() {
+  var temp;
+  
+  // if($("fdegrees").checked){
+  //   $("fInput").disabled = true
+  //   temp = fahrenheit(cvalue);
+  //   cvalue = temp;
+  // } else {
+  //   $("cInput").disabled = true;
+  // }
+  
+};
+
+let fahrenheit = function(celsius) {
+   return celsius * 1.8 + 32;
+}
+
+let celcius = function(fahrenheitDegree){
+  return parseFloat((fahrenheitDegree - 32)  * (5/9)).toFixed(2)
+}
+
+var control = function() {
+  
+   if($("fdegrees").checked){
+      $("fInput").disabled = false;
+      $("cInput").disabled = true;
+   } else {
+      $("cInput").disabled = false;
+      $("fInput").disabled = true;
+   }
+   
+  var fvalue = $("fInput").value;
+  var cvalue = $("cInput").value;
+  
+  if (cvalue){
+      console.log(fahrenheit(cvalue))
+    $("fInput").value = fahrenheit(cvalue);
+  } else if(fvalue){
+    console.log("empty")
+    $("cInput").value = celcius(fvalue);
+  }
+}
+
+
+window.addEventListener( "click", control)
